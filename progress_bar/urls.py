a6760cb3955f997404f2057c446c54119db9bd71
alt90 <alt90@rambler.ru>
2017-04-04 19:12:07 +0300

    Поправил работу статики

--- progress_bar/urls.py
+++ progress_bar/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 from achievement import views
+from progress_bar import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.StudentsListView.as_view(), name='students_list'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
